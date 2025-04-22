d328b3fc3e37e6b9b16cbf2b89c081dfdd39eee7
Pratyush Kumar Khadayate <khadayatepratyush@gmail.com>
2025-04-22 17:34:45 +0530

    problem is that balance is not showing in the pie chart problem is majorly due to charts folder or finance overview inside dashboard timestamps may be around 2 hrs 50 mins to 3 hrs

--- frontend/expense-tracker/src/pages/Dashboard/Home.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import DashboardLayout from "../../components/layouts/DashboardLayout";
 import { useUserAuth } from "../../hooks/useUserAuth";
 import { useNavigate } from "react-router-dom";
@@ -7,8 +7,11 @@ import axiosInstance from "../../utils/axiosInstance";
 import InfoCard from "../../components/Cards/InfoCard";
 import { LuHandCoins, LuWalletMinimal } from "react-icons/lu";
 import { addThousandsSeparator } from "../../utils/helper";
-import { IoMdCard } from 'react-icons/io';
+import { IoMdCard } from "react-icons/io";
 import RecentTransactions from "../../components/Dashboard/RecentTransactions";
+import FinanceOverview from "../../components/Dashboard/FinanceOverview";
+import ExpenseTransactions from "../../components/Dashboard/ExpenseTransactions";
+import Last30DaysExpenses from "../../components/Dashboard/last30DaysExpenses";
 
 const Home = () => {
   useUserAuth();
@@ -17,13 +20,14 @@ const Home = () => {
   const [dashboardData, setDashboardData] = useState(null);
   const [loading, setLoading] = useState(false);
   const fetchDashboardData = async () => {
-
     if (loading) return;
     setLoading(true);
     try {
       const response = await axiosInstance.get(
         `${API_PATHS.DASHBOARD.GET_DATA}`
       );
+
+      console.log("Fetched dashboard data:", response.data);
 
       if (response.data) {
         setDashboardData(response.data);
@@ -39,11 +43,12 @@ const Home = () => {
     fetchDashboardData();
     return () => {};
   }, []);
+  
 
   return (
     <DashboardLayout activeMenu="Dashboard">
       <div className="my-5 mx-auto">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        {/*<div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           <InfoCard
           icon = {<IoMdCard/>}
           label = "Total Balance"
@@ -65,13 +70,29 @@ const Home = () => {
           color = "bg-red-500"
           />
 
-        </div>
+        </div>*/}
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
-          <RecentTransactions
-            transactions = {dashboardData?.recentTransactions}
+          {/*<RecentTransactions
+            transactions={dashboardData?.recentTransactions}
             onSeeMore={() => navigate("/expense")}
-            />
+          />
+
+          <FinanceOverview
+            totalBalance={dashboardData?.totalBalance || 0}
+            totalIncome={dashboardData?.totalIncome || 0}
+            totalExpenses={dashboardData?.totalExpenses || 0}
+          />*/}
+        
+          <ExpenseTransactions
+          transactions={dashboardData?.last30DaysExpenses?.transactions || []}
+          onSeeMore = {() => navigate("/expense")}
+          />
+
+          <Last30DaysExpenses
+          data = {dashboardData?.last30DaysExpenses?.transactions || []}
+          />
+
         </div>
       </div>
     </DashboardLayout>
