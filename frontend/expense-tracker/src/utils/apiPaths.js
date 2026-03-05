8a829508f821e92aca3adbc575810491e5684385
Sujal Gangrade <sujalgangrade12@gmail.com>
2026-03-06 00:16:19 +0530

    naming correction

--- frontend/expense-tracker/src/utils/apiPaths.js
+++ frontend/expense-tracker/src/utils/apiPaths.js
@@ -1,6 +1,6 @@
-export const BASE_URL = "http://localhost:8000";
+// export const BASE_URL = "http://localhost:8000";
 // utils/apiPaths.js
-
+export const BASE_URL = "https://expensetracker-337k.onrender.com/" ;
 export const API_PATHS = {
   AUTH: {
     LOGIN: "/api/v1/auth/login",
