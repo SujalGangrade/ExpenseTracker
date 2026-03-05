ff66bf3a475dc74c531ec1260aa586971317b3f4
SujalGangrade <sujalgangrade12@gmail.com>
2026-03-05 23:39:22 +0530

    Update App.jsx

--- frontend/expense-tracker/src/App.jsx
+++ frontend/expense-tracker/src/App.jsx
@@ -10,7 +10,7 @@ import Home from "./pages/Dashboard/Home";
 import Expense from "./pages/Dashboard/Expense";
 import Income from "./pages/Dashboard/Income";
 import SignUp from "./pages/Auth/SignUp";
-import UserProvider from "./context/userContext";
+import UserProvider from "./context/UserContext";
 import { Toaster } from "react-hot-toast";
 
 const App = () => {
